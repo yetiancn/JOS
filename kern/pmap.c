ea9f446a1715dc4fd3a4002638ea2756205deda8
YeTian <yet@pku.edu.cn>
2018-03-17 16:53:54 +0800

    lab4e1

--- kern/pmap.c
+++ kern/pmap.c
@@ -620,7 +620,16 @@ mmio_map_region(physaddr_t pa, size_t size)
 	// Hint: The staff solution uses boot_map_region.
 	//
 	// Your code here:
-	panic("mmio_map_region not implemented");
+    void *ret;
+    ret = (void *) base;
+    size = ROUNDUP(size, PGSIZE);
+    if (base + size >= MMIOLIM)
+        panic("mmio_map_region: overflow MMIOLIM\n");
+    boot_map_region(kern_pgdir, base, size, pa, PTE_PCD | PTE_PWT | PTE_W);
+    base += size;
+    
+    return ret;
+    //panic("mmio_map_region not implemented");
 }
 
 static uintptr_t user_mem_check_addr;
